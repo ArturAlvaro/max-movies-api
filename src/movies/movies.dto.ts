7e3c9c7b1a9b02017adbb07b2d491702863fbdf7
ArturAlvaro <artur.alvaro73@gmail.com>
2024-06-06 18:00:37 -0300

    feat: add validação na criação de filmes

--- src/movies/movies.dto.ts
+++ src/movies/movies.dto.ts
@@ -1,11 +1,47 @@
+import {
+  IsDateString,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUUID,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class Movies {
+  @IsUUID()
+  @IsOptional()
   id: string;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(256)
   title: string;
+
+  @IsDateString()
   releaseYear: Date;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(256)
   description: string;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(256)
   genre: string;
+
+  @IsNumber()
   imdbRating: number;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(256)
   director: string;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(512)
   writers: string;
 }
 
