2065052f810622ff8ce5d1fcbcb7e19491dbc779
ArturAlvaro <artur.alvaro73@gmail.com>
2024-06-07 13:16:34 -0300

    feat: configura cache-manager

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './db/db.module';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), MoviesModule, UsersModule, AuthModule, DbModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    CacheModule.register({ isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    MoviesModule,
+    UsersModule,
+    AuthModule,
+    DbModule,
+  ],
 })
 export class AppModule {}
