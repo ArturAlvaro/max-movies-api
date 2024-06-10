5a5a7e4d06612ca80605d9e748e1537de2a43dd2
ArturAlvaro <artur.alvaro73@gmail.com>
2024-06-10 20:01:14 -0300

    test: cria teste de controller para usuario

---
+++ src/users/users.controller.spec.ts
@@ -0,0 +1,55 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { UserEntity } from 'src/db/entities/user.entity';
+import { v4 as uuid } from 'uuid';
+import { UserDto } from './users.dto';
+import { ConflictException, NotFoundException } from '@nestjs/common';
+
+const userEntity: UserEntity = {
+  id: uuid(),
+  active: true,
+  username: 'jhon.doe@email.com',
+  password: 'teste123',
+};
+
+describe('UserController', () => {
+  let userController: UsersController;
+  let userService: UsersService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {
+            create: jest
+              .fn()
+              .mockResolvedValue({ id: userEntity.id, username: userEntity.username }),
+            findByUserName: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    userController = module.get<UsersController>(UsersController);
+    userService = module.get<UsersService>(UsersService);
+  });
+
+  it('Testa rota de criação de usuário', () => {
+    expect(userController).toBeDefined();
+    expect(userService).toBeDefined();
+  });
+
+  it('Retorna id e username', async () => {
+    // act
+    const result = await userController.create(userEntity);
+
+    // assert
+    expect(result).toHaveProperty('id');
+    expect(result).toHaveProperty('username');
+    expect(result.username).toEqual(userEntity.username);
+    expect(userService.create).toHaveBeenCalledTimes(1);
+  });
+});
