b08264d204d03e1fc69a4d465e918c4072594ce2
ArturAlvaro <artur.alvaro73@gmail.com>
2024-06-20 11:53:20 -0300

    feat: add log e altera importaçao de entidades

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -1,21 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { MovieEntity } from './entities/movie.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: +configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        entities: [__dirname + '/entities/**'],
-        migrations: [__dirname + '/migrations/*.ts'],
-        synchronize: false,
-      }),
+      useFactory: async (configService: ConfigService) => {
+        const dbName = configService.get<string>('DB_NAME');
+        console.log(`Connecting to database: ${dbName}`);
+
+        return {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST'),
+          port: +configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_NAME'),
+          entities: [UserEntity, MovieEntity],
+          migrations: [__dirname + '/migrations/*.ts'],
+          synchronize: false,
+        };
+      },
       inject: [ConfigService],
     }),
   ],
