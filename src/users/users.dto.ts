fb088e3c35d16ab7b67dc65e79029cae24e6443c
ArturAlvaro <artur.alvaro73@gmail.com>
2024-06-20 12:21:28 -0300

    feat: altera arquivo

--- src/users/users.dto.ts
+++ src/users/users.dto.ts
@@ -1,6 +1,14 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class UserDto {
   id: string;
+
+  @IsString()
+  @IsEmail()
   username: string;
+
+  @IsString()
   password: string;
+
   active: boolean;
 }
