95a2a181e8461e49f3e41657c3efc8b5e5ac47a6
ArturAlvaro <artur.alvaro73@gmail.com>
2024-06-20 21:37:09 -0300

    doc: documentação da auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,21 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
-import { AuthResponseDto } from './auth.dto';
+import { AuthResponseDto, AuthDto } from './auth.dto';
 import { AuthService } from './auth.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { authSwagger } from './swagger/auth.swagger';
+import { badRequestSwagger } from 'helpers/badRequest.swagger';
 
 @Controller('auth')
+@ApiTags('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  async signIn(
-    @Body('username') username: string,
-    @Body('password') password: string
-  ): Promise<AuthResponseDto> {
-    return await this.authService.signIn(username, password);
+  @ApiOperation({ summary: 'Autenticação do usuário' })
+  @ApiResponse({ status: 200, description: 'Usuário autenticado', type: authSwagger })
+  @ApiResponse({ status: 401, description: 'Dados incorretos', type: badRequestSwagger })
+  async signIn(@Body() authDto: AuthDto): Promise<AuthResponseDto> {
+    return await this.authService.signIn(authDto.username, authDto.password);
   }
 }
