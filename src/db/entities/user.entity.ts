f511b5728feaef4deebe92a544af3fc77e5b5b72
ArturAlvaro <artur.alvaro73@gmail.com>
2024-06-20 21:37:55 -0300

    doc: configura doc na entidade usuario

--- src/db/entities/user.entity.ts
+++ src/db/entities/user.entity.ts
@@ -1,18 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
 
 @Entity({ name: 'user' })
 @Unique(['username'])
 export class UserEntity {
   @PrimaryGeneratedColumn('uuid')
+  @ApiProperty()
   id: string;
 
   @Column({ type: 'varchar' })
+  @ApiProperty()
   username: string;
 
   @Column({ type: 'varchar' })
+  @ApiProperty()
   password: string;
 
   @Column({ type: 'boolean', default: true })
+  @ApiProperty()
   active: boolean;
 
   constructor(user?: Partial<UserEntity>) {
