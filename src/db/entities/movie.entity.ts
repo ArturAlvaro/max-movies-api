b7103d55303546456dba6e1cc5ed95aa5672374d
ArturAlvaro <artur.alvaro73@gmail.com>
2024-06-20 22:31:31 -0300

    doc: documenta entidade de filmes

--- src/db/entities/movie.entity.ts
+++ src/db/entities/movie.entity.ts
@@ -1,28 +1,37 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'movie' })
 export class MovieEntity {
   @PrimaryGeneratedColumn('uuid')
+  @ApiProperty()
   id: string;
 
   @Column({ type: 'varchar' })
+  @ApiProperty()
   title: string;
 
   @Column({ type: 'timestamptz', name: 'releaseyear' })
+  @ApiProperty()
   releaseYear: Date;
 
   @Column({ type: 'varchar' })
+  @ApiProperty()
   description: string;
 
   @Column({ type: 'varchar' })
+  @ApiProperty()
   genre: string;
 
   @Column({ type: 'numeric', name: 'imdbrating' })
+  @ApiProperty()
   imdbRating: number;
 
   @Column({ type: 'varchar' })
+  @ApiProperty()
   director: string;
 
   @Column({ type: 'varchar' })
+  @ApiProperty()
   writers: string;
 }
